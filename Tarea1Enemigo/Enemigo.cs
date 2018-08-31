0c994bbf7157a2f9e0e7e8f9eaacb8a07b15490b
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-31 07:22:22 -0700

    Agregar archivos de proyecto.

---
+++ Tarea1Enemigo/Enemigo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tarea1Enemigo
+{
+    class Enemigo
+    {
+        public string posicionX;
+        public string posicionY;
+        public string xp;
+        public string disparos;
+        public string movimiento;
+        public string explotar;
+
+    }
+}
---
+++ Tarea1Enemigo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tarea1Enemigo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemigo enemigo1 = new Enemigo();
+            enemigo1.posicionX = "15";
+            enemigo1.posicionY = "20";
+            enemigo1.xp = "250";
+            enemigo1.movimiento = "derecha";
+            enemigo1.disparos = "4";
+            enemigo1.explotar = "grande";
+        }
+    }
+}
